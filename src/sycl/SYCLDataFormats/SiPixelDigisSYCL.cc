3befac790356d0b50fd6591f9febbab04ee898b8
Aurora Perego <84700885+AuroraPerego@users.noreply.github.com>
2022-07-17 23:39:08 +0200

    [sycl] added plugin-SIPixelRawToDigi and dependencies

---
+++ src/sycl/SYCLDataFormats/SiPixelDigisSYCL.cc
@@ -0,0 +1,55 @@
+#include "SYCLDataFormats/SiPixelDigisSYCL.h"
+
+#include "SYCLCore/device_unique_ptr.h"
+#include "SYCLCore/host_unique_ptr.h"
+//#include "SYCLCore/copyAsync.h" check if it works w/o this, using directly memcpy
+
+SiPixelDigisSYCL::SiPixelDigisSYCL(size_t maxFedWords, sycl::queue stream) {
+  xx_d = cms::sycltools::make_device_unique<uint16_t[]>(maxFedWords, stream);
+  yy_d = cms::sycltools::make_device_unique<uint16_t[]>(maxFedWords, stream);
+  adc_d = cms::sycltools::make_device_unique<uint16_t[]>(maxFedWords, stream);
+  moduleInd_d = cms::sycltools::make_device_unique<uint16_t[]>(maxFedWords, stream);
+  clus_d = cms::sycltools::make_device_unique<int32_t[]>(maxFedWords, stream);
+
+  pdigi_d = cms::sycltools::make_device_unique<uint32_t[]>(maxFedWords, stream);
+  rawIdArr_d = cms::sycltools::make_device_unique<uint32_t[]>(maxFedWords, stream);
+
+  auto view = cms::sycltools::make_host_unique<DeviceConstView>(stream);
+  view->xx_ = xx_d.get(); 
+  view->yy_ = yy_d.get();
+  view->adc_ = adc_d.get();
+  view->moduleInd_ = moduleInd_d.get();
+  view->clus_ = clus_d.get();
+
+  view_d = cms::sycltools::make_device_unique<DeviceConstView>(stream);
+  stream.memcpy(view_d.get(), view.get(), sizeof(DeviceConstView));
+  //cms::sycltools::copyAsync(view_d, view, stream);
+}
+
+cms::sycltools::host::unique_ptr<uint16_t[]> SiPixelDigisSYCL::adcToHostAsync(sycl::queue stream) const {
+  auto ret = cms::sycltools::make_host_unique<uint16_t[]>(nDigis(), stream);
+  stream.memcpy(ret.get(), adc_d.get(), nDigis() * sizeof(uint16_t[]));
+  //cms::sycltools::copyAsync(ret, adc_d, nDigis(), stream);
+  return ret;
+}
+
+cms::sycltools::host::unique_ptr<int32_t[]> SiPixelDigisSYCL::clusToHostAsync(sycl::queue stream) const {
+  auto ret = cms::sycltools::make_host_unique<int32_t[]>(nDigis(), stream);
+  stream.memcpy(ret.get(), clus_d.get(), nDigis() * sizeof(int32_t[]));
+  //cms::sycltools::copyAsync(ret, clus_d, nDigis(), stream);
+  return ret;
+}
+
+cms::sycltools::host::unique_ptr<uint32_t[]> SiPixelDigisSYCL::pdigiToHostAsync(sycl::queue stream) const {
+  auto ret = cms::sycltools::make_host_unique<uint32_t[]>(nDigis(), stream);
+  stream.memcpy(ret.get(), pdigi_d.get(), nDigis() * sizeof(uint32_t[]));
+  //cms::sycltools::copyAsync(ret, pdigi_d, nDigis(), stream);
+  return ret;
+}
+
+cms::sycltools::host::unique_ptr<uint32_t[]> SiPixelDigisSYCL::rawIdArrToHostAsync(sycl::queue stream) const {
+  auto ret = cms::sycltools::make_host_unique<uint32_t[]>(nDigis(), stream);
+  stream.memcpy(ret.get(), rawIdArr_d.get(), nDigis() * sizeof(uint32_t[]));
+  //cms::sycltools::copyAsync(ret, rawIdArr_d, nDigis(), stream);
+  return ret;
+}
