1dc9df2b3947db9a87fa6eef9568473737d463aa
Nikolaos Andriotis <nandriot@olice-05.cern.ch>
2022-08-11 20:55:35 +0200

    This was a long work day... This commit has the correct atomics and correct accessor sizes. I tried after a lot of developing to debug in CPU and it is not working as expected..

--- src/sycl/SYCLCore/syclAtomic.h
+++ src/sycl/SYCLCore/syclAtomic.h
@@ -16,10 +16,78 @@ namespace cms {
       return first.fetch_add(j);
     }
 
+    template <typename T>
+    inline T atomic_fetch_add(T* addr, T operand){
+      auto atm = 
+            cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, sycl::memory_scope::device, 
+                                  cl::sycl::access::address_space::global_space > (addr[0]);
+      return atm.fetch_add(operand);
+    }
+
+    template <typename T>
+    inline T atomic_fetch_add_shared(T* addr, T operand){
+      auto atm = 
+            cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, sycl::memory_scope::device, 
+                                  cl::sycl::access::address_space::local_space> (addr[0]);
+      return atm.fetch_add(operand);
+    }
+
     template <typename A, typename B>
     inline A AtomicSub(A* i, B j){
       sycl::atomic_ref<A, sycl::memory_order::relaxed, sycl::memory_scope::work_group> first(*i);
       return first.fetch_add(-j);
+    }
+
+  template <typename T>
+  inline T atomic_fetch_compare_inc(T *addr,T operand) {
+  auto atm = cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, 
+                                                cl::sycl::memory_scope::device,
+                                   cl::sycl::access::address_space::global_space>(addr[0]);
+  T old;
+  while (true) {
+    old = atm.load();
+    if (old >= operand) {
+      if (atm.compare_exchange_strong(old, 0))
+        break;
+    } else if (atm.compare_exchange_strong(old, old + 1))
+      break;
+  }
+  return old;
+}
+
+  template <typename T>
+    inline T atomic_fetch_compare_inc_shared(T *addr,T operand) {
+    auto atm = cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, 
+                                                  cl::sycl::memory_scope::device,
+                                     cl::sycl::access::address_space::local_space>(addr[0]);
+    T old;
+    while (true) {
+      old = atm.load();
+      if (old >= operand) {
+        if (atm.compare_exchange_strong(old, 0))
+          break;
+      } else if (atm.compare_exchange_strong(old, old + 1))
+        break;
+    }
+    return old;
+  }
+
+    template <typename T>
+    inline T atomic_fetch_min_shared(T *addr, T operand) {
+    auto atm =
+      cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, cl::sycl::memory_scope::device,
+                              cl::sycl::access::address_space::local_space>
+                              (addr[0]);            
+    return atm.fetch_min(operand);
+    }
+
+    template <typename T>
+    inline T atomic_fetch_min(T *addr, T operand) {
+    auto atm =
+      cl::sycl::atomic_ref<T, cl::sycl::memory_order::relaxed, cl::sycl::memory_scope::device,
+                              cl::sycl::access::address_space::global_space>
+                              (addr[0]);            
+    return atm.fetch_min(operand);
     }
 
     template <typename A, typename B>
@@ -27,7 +95,8 @@ namespace cms {
       sycl::ext::oneapi::atomic_ref<A, sycl::ext::oneapi::memory_order::relaxed,
                                 sycl::ext::oneapi::memory_scope::work_group,
                                 sycl::access::address_space::local_space> first(*i);
-      return first.fetch_min(j);
+      auto test = first.fetch_min(j);
+      return test;
       //sycl::atomic<A>(sycl::global_ptr<A>(i)).fetch_min(j);
     }
 
@@ -45,6 +114,22 @@ namespace cms {
       auto ret = *i;
       sycl::atomic_ref<A, sycl::memory_order::relaxed, sycl::memory_scope::work_group> first(*i);
       first.fetch_add(-j);
+      if (*i < 0){
+          sycl::atomic_ref<A, sycl::memory_order::relaxed, sycl::memory_scope::work_group> second(*i);
+          second.fetch_add(1);
+      }
+      first.fetch_add(j);
+      return ret;
+    }
+
+ template <typename A, typename B>
+    inline A AtomicInc2(A* i, B j, sycl::stream out){
+      auto ret = *i;
+      //out << "The value of i before is :" << *i << "\n";
+      sycl::atomic_ref<A, sycl::memory_order::relaxed, sycl::memory_scope::work_group> first(*i);
+      first.fetch_add(-j);
+      //out << "The value of fetchadd is :" <<  first.fetch_add(-j) << "\n";
+      //out << "The value of i is :" << *i << "\n";
       if (*i < 0){
           sycl::atomic_ref<A, sycl::memory_order::relaxed, sycl::memory_scope::work_group> second(*i);
           second.fetch_add(1);
