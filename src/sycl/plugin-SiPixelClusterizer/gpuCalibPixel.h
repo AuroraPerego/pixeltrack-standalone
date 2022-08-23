69fd7fbfa1ac778547a367f2be52a53bd7fd0027
Nikolaos Andriotis <nandriot@olice-05.cern.ch>
2022-08-23 18:12:08 +0200

    first complete working version on CPU, GPU out of resources, not able to run more than 9 events

--- src/sycl/plugin-SiPixelClusterizer/gpuCalibPixel.h
+++ src/sycl/plugin-SiPixelClusterizer/gpuCalibPixel.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <iomanip>
 
 #include "CondFormats/SiPixelGainForHLTonGPU.h"
 #include "SYCLCore/sycl_assert.h"
@@ -65,10 +66,10 @@ namespace gpuCalibPixel {
         out << "bad pixel at " << i << " in " << id[i] << "\n";
       } else {
         float vcal = adc[i] * gain - pedestal * gain;
-        if (vcal < 0)
-        out << "adc was :" << adc[i] << " gain was :" << gain << " pedestal was :"<< pedestal << "\n";
-
-        adc[i] = std::max(100, int(vcal * conversionFactor + offset));
+        if (i==47848){
+        out << "adc[47848]  was : "<< sycl::stream_manipulator::fixed << sycl::setprecision(10) <<  adc[47848] << "gain was : " << gain << "pedestal was :  " <<  pedestal << "\n";
+        out << "vcal was : "<< sycl::stream_manipulator::fixed << sycl::setprecision(10) <<vcal << "conversionFactor was : " << conversionFactor << "offset was :  " <<  offset << "\n";}
+        adc[i] = sycl::max(100, int(vcal * conversionFactor + offset));
       }
     }
   }
