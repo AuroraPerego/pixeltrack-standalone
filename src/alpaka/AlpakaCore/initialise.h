5ca2fb21336e4328664711d8bdafe2399480f99c
Andrea Bocci <andrea.bocci@cern.ch>
2023-08-22 17:38:08 +0200

    [alpaka] Fix ODR issue with platform objcts
    Fix an ODR violation in the definitaion of the cms::alpakatools::platform global
    variables:
      - template the platform global variable on the Platform type;
      - move the declaration of platform from AlpakaCore/getDeviceIndex.h to
        AlpakaCore/alpakaDevices.h;
      - move the initialisation of the platform to cms::alpakatools::initialise().
    
    Other improvements:
      - replace cms::alpakatools::enumerate() with alpaka::getDevs().

--- src/alpaka/AlpakaCore/initialise.h
+++ src/alpaka/AlpakaCore/initialise.h
@@ -5,36 +5,36 @@
 
 namespace cms::alpakatools {
 
+  // note: this function is not thread-safe
   template <typename TPlatform>
-  void initialise();
+  void initialise(bool verbose = false);
 
   template <typename TPlatform>
   void resetDevices();
 
   // explicit template instantiation declaration
 #ifdef ALPAKA_ACC_CPU_B_SEQ_T_SEQ_PRESENT
-  extern template void initialise<alpaka_serial_sync::Platform>();
+  extern template void initialise<alpaka_serial_sync::Platform>(bool);
   extern template void resetDevices<alpaka_serial_sync::Platform>();
 #endif
 #ifdef ALPAKA_ACC_CPU_B_TBB_T_SEQ_PRESENT
-  extern template void initialise<alpaka_tbb_async::Platform>();
+  extern template void initialise<alpaka_tbb_async::Platform>(bool);
   extern template void resetDevices<alpaka_tbb_async::Platform>();
 #endif
 #ifdef ALPAKA_ACC_GPU_CUDA_PRESENT
-  extern template void initialise<alpaka_cuda_async::Platform>();
+  extern template void initialise<alpaka_cuda_async::Platform>(bool);
   extern template void resetDevices<alpaka_cuda_async::Platform>();
 #endif
 #ifdef ALPAKA_ACC_GPU_HIP_PRESENT
-  extern template void initialise<alpaka_rocm_async::Platform>();
+  extern template void initialise<alpaka_rocm_async::Platform>(bool);
   extern template void resetDevices<alpaka_rocm_async::Platform>();
 #endif
 #ifdef ALPAKA_ACC_SYCL_PRESENT
-  extern template void initialise<alpaka_cpu_sycl::Platform>();
-  extern template void initialise<alpaka_gpu_sycl::Platform>();
+  extern template void initialise<alpaka_cpu_sycl::Platform>(bool);
+  extern template void initialise<alpaka_gpu_sycl::Platform>(bool);
   extern template void resetDevices<alpaka_cpu_sycl::Platform>();
   extern template void resetDevices<alpaka_gpu_sycl::Platform>();
 #endif
-
 }  // namespace cms::alpakatools
 
 #endif  // AlpakaCore_initialise_h
